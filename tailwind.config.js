2ebe7aea1030598acfb0928aa851e9258b5fc93e
lovemage <support@oceanads.org>
2025-12-08 01:37:47 +0800

    Fix Tailwind CSS v4 configuration - use new @import syntax

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./App.tsx",
+    "./index.tsx",
   ],
   theme: {
     extend: {},
